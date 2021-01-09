717cc4cf5a169784d2d9616e27b61322ffc19b29
simpsons01 <ilikekobe01@gmail.com>
2021-01-09 23:15:20 +0800

    add vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img :src="env.VUE_APP_STATIC_PATH + 'logo.png'">
+    <p>pratice ci/cd and s3 deploy</p>
   </div>
 </template>
 
 <script>
+import env from "@/utility/env"
+
 export default {
   name: 'App',
+  data() {
+    return {
+      env
+    }  
+  }
 }
 </script>
+
+
+<style>
+body, html {
+  height: 100%;
+  min-height: 100%;
+}
+
+#app > p {
+  color: red
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const isDev = process.env.VUE_APP_ENV === "development"
+
+module.exports = {
+  chainWebpack: config => {
+    if(!isDev) {
+      config.plugins.delete('html')
+      config.plugins.delete('preload')
+      config.plugins.delete('prefetch')
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: `[name].[${isDev ? 'hash' : 'contenthash'}].js`,
+      chunkFilename: `[name].[${isDev ? 'hash' : 'contenthash'}].js` 
+    }
+  },
+  css: {
+    extract: !isDev
+  }
+}
