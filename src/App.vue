e9b723bfc98aa1f44e5a32086a08a4514b36b98f
simpsosns01 <ilikekobe01@gmail.com>
2021-01-15 19:03:36 +0800

    remove yarn and use npm

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,15 @@
   <div id="app">
     <img :src="env.VUE_APP_STATIC_PATH + 'img/logo.png'" />
     <p>{{ gender }}</p>
-    <Foo />
-    <Foo2 />
   </div>
 </template>
 3
 <script>
 import env from "@/utility/env"
-import Foo from "@/component/Foo"
-import Foo2 from "@/component/Foo2"
 
 export default {
   name: "App",
-  components: {
-    Foo,
-    Foo2
-  },
+  components: {},
   data() {
     return {
       env,
@@ -32,6 +25,7 @@ export default {
 </style>
 <style lang="scss" scoped>
 @import "~@/scss/variables";
+
 #app {
   p {
     color: $primary;
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import VueRouter from "vue-router"
+
+const routes = []
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
