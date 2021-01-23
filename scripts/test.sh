1993d2f88295fe5b1fd89e62541bb07b9376fe8d
simpsons01 <ilikekobe01@gmail.com>
2021-01-23 14:24:59 +0800

    remove shell script

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "start test phase"
-
-if { [ "$TRAVIS_BRANCH" == "lab" ] || [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "prod" ]; } && 
-   [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
-yarn lint;
-fi
-
-exit 0
